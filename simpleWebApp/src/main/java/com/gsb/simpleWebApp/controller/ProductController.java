d1a29699975022f91bd7393fd6f0efe0cf4b359b
Gagan <bhatia.gagandeepsingh61@gmail.com>
2024-08-23 00:28:37 +0530

    MVC Model --> Data Service --> Logic
    GET (R) -- Get the data from Server
    POST (C) -- Add data to server
    PUT (U) -- Update existing record
    DELETE (D) -- Delete record
    CRUD

--- simpleWebApp/src/main/java/com/gsb/simpleWebApp/controller/ProductController.java
+++ simpleWebApp/src/main/java/com/gsb/simpleWebApp/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.gsb.simpleWebApp.controller;
 import com.gsb.simpleWebApp.model.Product;
 import com.gsb.simpleWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +16,27 @@ public class ProductController {
     @RequestMapping("/products")
     public List<Product> getProducts() {
         return productService.getProducts();
+    }
+
+    @RequestMapping("/products/{prodId}")
+    public Product getProductById (@PathVariable int prodId) {
+        return productService.getProductById(prodId);
+    }
+
+    @PostMapping("products")
+    public void addProduct (@RequestBody Product product) {
+        System.out.println(product);
+        productService.addProduct(product);
+    }
+
+    @PutMapping("products")
+    public void updateProduct (@RequestBody Product product) {
+        productService.updateProduct(product);
+    }
+
+    @DeleteMapping("products/{productId}")
+    public void deleteProduct (@PathVariable int productId) {
+        productService.deleteProduct(productId);
     }
 
 }
--- simpleWebApp/src/main/java/com/gsb/simpleWebApp/service/ProductService.java
+++ simpleWebApp/src/main/java/com/gsb/simpleWebApp/service/ProductService.java
@@ -3,19 +3,50 @@ package com.gsb.simpleWebApp.service;
 import com.gsb.simpleWebApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
       new Product(101, "Iphone", 50000),
       new Product(102, "IQ00", 60000)
-    );
+    ));
 
     public List<Product> getProducts() {
         return products;
     }
 
+    public Product getProductById (int prodId) {
+        return products.stream()
+                .filter(p -> p.getProdId() == prodId)
+                .findFirst().get();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void updateProduct(Product product) {
+        int index = 0;
+        for (int i = 0; i < products.size(); i++)
+            if (products.get(i).getProdId() == product.getProdId())
+                index = i;
+
+        products.set(index, product);
+
+    }
+
+    public void deleteProduct(int productId) {
+
+        int index = 0;
+        for (int i = 0; i < products.size(); i++)
+            if (products.get(i).getProdId() == productId)
+                index = i;
+
+        products.remove(index);
+
+    }
 }
